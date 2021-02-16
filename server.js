29898c9c4879f78a228ffaa9665e1e23301cf051
Destiny Fleming <destiny.fleming7401@gmail.com>
2021-02-15 18:02:13 -0800

    made the pages handlebars

--- server.js
+++ server.js
@@ -1,1 +1,13 @@
-const express = require ('express')
+const express = require ('express');
+var exphbs = require("express-handlebars");
+
+var app = express();
+
+var PORT = process.env.PORT || 8080;
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+app.listen(PORT, function() {
+    console.log("Server listening on: http://localhost:" + PORT);
+  });
