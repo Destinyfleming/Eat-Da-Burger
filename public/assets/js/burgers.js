6b32bae6de88784278b7b15dc76ee9a6c7fe4cb2
Destiny Fleming <destiny.fleming7401@gmail.com>
2021-04-09 12:51:45 -0700

    added styling, fixing a bug in js

--- public/assets/js/burgers.js
+++ public/assets/js/burgers.js
@@ -21,7 +21,7 @@ $(function () {
     event.preventDefault();
 
     var newBurger = {
-      burger_name: $("#brgr").val().trim(),
+      burger_name: $("#burger").val().trim(),
       devoured: $("[name=devoured]:checked").val().trim(),
     };
 
