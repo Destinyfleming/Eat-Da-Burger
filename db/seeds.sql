c84fb431130903a3beb75ce872c0094d77f04b37
Destiny Fleming <destiny.fleming7401@gmail.com>
2021-04-09 09:54:31 -0700

    restart to push to heroku

--- db/seeds.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO burgers (burger_name, devoured) VALUES ('Cheeseburger', '0');
-INSERT INTO burgers (burger_name, devoured) VALUES ('Hamburger', '1');
-INSERT INTO burgers (burger_name, devoured) VALUES ('Veggie Burger', '0');
