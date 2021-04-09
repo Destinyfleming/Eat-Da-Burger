8e559ed1b4367983e8bce10e5b26ddb1deb4d0e6
Destiny Fleming <destiny.fleming7401@gmail.com>
2021-04-09 08:57:51 -0700

    working out bug

--- config/connection.js
+++ config/connection.js
@@ -1,6 +1,6 @@
 const mysql = require("mysql");
 let connection;
-
+//mysql://itt0t86ptsj4b9eb:p5al24r2dtxjwneh@eyw6324oty5fsovx.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/ikq981mt2tpc7ycs
 if(process.env.JAWSDB_URL) {
 	connection = mysql.createConnection(process.env.JAWSDB_URL);
   }
@@ -9,7 +9,7 @@ if(process.env.JAWSDB_URL) {
 	host: "localhost",
 	port: 3306,
 	user: "root",
-	password: "",
+	password: "Desi7401",
 	database: "burger_db"
   });
 }
